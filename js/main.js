c9375b0addbcb1608c8bdc573891df7756996948
hudu249 <duchuynh249@gmail.com>
2021-10-05 17:52:40 +0700

    kkk
    kkk

--- js/main.js
+++ js/main.js
@@ -7,13 +7,28 @@ $(document).ready(function(){
 
     $(window).on('load scroll',function(){
         $('.fa-bars').removeClass('fa-times');
-        $('.navbar').removeClass('nav-toggle'); 
-        
-        if($(window).scrollTop() > 30){
-            $('.headin').css({'background':'#0FCBDA','box-shadow':'0 .2rem .5rem rgba(0,0,0,.4'});
+        $('.nav-bar').removeClass('nav-toggle'); 
+              
+        if($(window).scrollTop() > '20'){
+            $('.heading').css({'background':'#0FCBDA','box-shadow':'0 .2rem .5rem rgba(0,0,0,.4'});
         
         }else{
-            $('.headin').css({'background':'none','box-shadow':'none'});
+            $('.heading').css({'background':'none','box-shadow':'none'});
         }
     });
 });
+
+
+$("img.lazy").lazyload();
+
+
+wow = new WOW(
+    {
+    boxClass:     'wow',      // default
+    animateClass: 'animated', // default
+    offset:       0,          // default
+    mobile:       true,       // default
+    live:         true        // default
+  }
+  )
+  wow.init();
